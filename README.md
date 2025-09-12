fa55dc2bd9f967feb129441d3e96d84bcdd17e68
JuanPablo990 <juan.nieto-co@mail.escuelaing.edu.co>
2025-09-11 21:13:25 -0500

    feat: Parcial parte 1 - creacion del proyecto

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Parcial-Corte-1-CVDS-DOSW
 Parcial del Corte 1
+
+## Integrantes:
+- juan Pablo Nieto Cortes
+
+---
+
+## maven corriendo bien
+
+![img.png](docs/imagenes/img.png)
+
+## Maven Test
+
+![img.png](docs/imagenes/img.png)
---
+++ src/test/java/eci/edu/dosw/parcial/Parcial_Corte_1_CVDS/DOSW/ApplicationTests.java
@@ -0,0 +1,13 @@
+package eci.edu.dosw.parcial.Parcial_Corte_1_CVDS.DOSW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
